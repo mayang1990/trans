2ba9d89ecea6ca077f25ac3d0fa723494fcf5d49
mayang1990 <mayang_toutou@126.com>
2014-10-15 17:15:57 +0800

    表示出来了，可是有点乱，利用的是字符串。在考虑字符串数组，再想想。

--- src/trans/trans.java
+++ src/trans/trans.java
@@ -1,22 +1,34 @@
 package trans;
 
+import java.util.ArrayList;
+
 public class trans{
 	  
 	  public static void main(String args[]){
-		String first = " @你好 @喜欢你 @大家好";
-		int i= 0;
+		String first = " 你好 @喜欢你 @大家好 :";
+		int i= 0,m = 0;
+		ArrayList list = new ArrayList();
 		char c = first.charAt(i);
-		while( i != first.length() - 1 )
+		while( i != first.length() - 1 && c != ':')
 		{
+			list.add(c);
 			if( c == '@')
 			{
-				 System.out.println("hello");
-				 int j = i;
-				 System.out.println(j);
+				for(int j=0;j<list.size()-1;++j)
+				 {
+					 System.out.print(list.get(j));
+				 
+				}
+				 list.removeAll(list);
+				 System.out.print(" 转发 ");
+				
 			}
 			 c = first.charAt(++i);
-		}
-		
-	   
+		 }
+		 for(int j=0;j<list.size();++j)
+		 {
+			 System.out.print(list.get(j));
+		 }
+		}	
+	 
 	  }
-	}
