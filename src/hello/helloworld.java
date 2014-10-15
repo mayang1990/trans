63260e1a94cff9589da8bf210fbe9936c28c9f88
mayang1990 <mayang_toutou@126.com>
2014-10-15 11:18:06 +0800

    修改

--- src/hello/helloworld.java
+++ src/hello/helloworld.java
@@ -4,6 +4,6 @@ public class helloworld{
 	  // 程序的入口
 	  public static void main(String args[]){
 	    // 向控制台输出信息
-	    System.out.println("欢迎java01班的同学");
+	    System.out.println("hello");
 	  }
 	}
